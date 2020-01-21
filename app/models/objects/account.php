110a3bb25472063f42ff450922cfa445fc89e716
Dakpaneel <bradleyoosterveen@gmail.com>
2020-01-21 08:39:53 +0100

    add functions to add account

--- app/models/objects/account.php
+++ app/models/objects/account.php
@@ -32,7 +32,40 @@
 
         }
 
+        function getSingle() {
+
+            $sql = $this->conn->prepare("
+                SELECT `username` FROM `users` WHERE `username` = :username
+            ");
+
+            $sql->bindParam(':username', $this->username);
+
+            if (!$sql->execute()) return false;
+
+            if (empty($res = $sql->fetchall())) return false;
+
+            return $res;
+
+        }
+
         function add() {
+
+            $sql = $this->conn->prepare("
+                INSERT INTO `users` 
+                    (`username`, `firstname`, `lastname`, `password`, `created_on`, `last_edited`, `last_login`)
+                VALUES 
+                    (:username, :firstname, :lastname, :password, current_timestamp(), current_timestamp(), NULL);
+            ");
+
+            $sql->bindParam(':username', $this->username);
+            $sql->bindParam(':firstname', $this->firstname);
+            $sql->bindParam(':lastname', $this->lastname);
+            $sql->bindParam(':password', $this->password);
+
+            if (!$sql->execute()) return false;
+
+            return true;
+
 
         }
 
--- app/views/pages/accounts/add.php
+++ app/views/pages/accounts/add.php
@@ -5,4 +5,70 @@
 
     $account = new Account($db);
 
+    if (isset($_POST['add'])) {
+
+        $account->username = htmlspecialchars($_POST["username"]);
+        $account->firstname = htmlspecialchars($_POST["firstname"]);
+        $account->lastname = htmlspecialchars($_POST["lastname"]);
+        $account->password = htmlspecialchars(password_hash($_POST["password"], PASSWORD_DEFAULT));
+
+        if (!$account->getSingle()) {
+
+            if ($account->add()) header("location:/accounts");
+
+        }
+
+    }
+
 ?>
+
+<main>
+    <div class="wrapper">
+        <div class="head main-head">
+            <div class="row">
+                <div class="col">
+                    <h1><?=$page_title?></h1>
+                </div>
+            </div>
+        </div>
+        <div class="body">
+
+        <form action="" method="POST">
+            <div class="wrapper">
+                <div class="body">
+
+                    <div class="input">
+                        <label for="firstname"><span>Voornaam</span></label>
+                        <input type="text" name="firstname" placeholder="Jack">
+                        <div class="line focus"></div>
+                    </div>
+
+                    <div class="input">
+                        <label for="lastname"><span>Achternaam</span></label>
+                        <input type="text" name="lastname" placeholder="Danger">
+                        <div class="line focus"></div>
+                    </div>
+
+                    <div class="input">
+                        <label for="username"><span>Gebruikersnaam</span></label>
+                        <input type="text" name="username" placeholder="Noobmaster69">
+                        <div class="line focus"></div>
+                    </div>
+
+                    <div class="input">
+                        <label for="password"><span>Password</span></label>
+                        <input type="password" name="password" placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;">
+                        <div class="line focus"></div>
+                    </div>
+
+                    <div class="row">
+                        <input type="submit" name="add" value="<?=$page_title?>">
+                    </div>
+
+                </div>
+            </div>
+        </form>
+
+        </div>
+    </div>
+</main>
